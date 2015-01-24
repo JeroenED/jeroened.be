7e8340d8b9780c91e9ae3ba97325e4d23f3a68c8
Jeroen De Meerleer <jeroen.de.meerleer@telenet.be>
2015-01-24 18:19:38 +0100

    Reformat

--- js/jeroened.js
+++ js/jeroened.js
@@ -1,36 +1,36 @@
 $(document).ready(function() {
-    
 	$("nav").click(function() {
 		var cur = $("nav ul li").css("margin-left");
-		
-		if (cur == "0px") { cur = "200px" }
-		else { cur = "0px" }
-		$("nav ul li").animate({ "margin-left": cur });
+		if (cur == "0px") {
+			cur = "200px"
+		} else {
+			cur = "0px"
+		}
+		$("nav ul li").animate({
+			"margin-left": cur
+		});
 	});
-	
 	$("a").click(function() {
 		var page = $(this).data("page");
 		OpenPage(page);
 	});
-	
-	$('.page').click(function(event){
-   if(!$(event.target).is('form *')){
-	   $(".page").css("display", "none");
-   }
-});
-
+	$('body').click(function(event) {
+		if($('.page span.closeable')[0]) {
+			$(".page").css("display", "none");
+			$("span.closeable").remove();
+		}
+	});
 });
 
 function OpenPage(page) {
-	var element = $("#" + page)	;
-	
+	var element = $("#" + page);
 	element.css("display", "block");
-	element.css("position","absolute");
-	element.css("max-height",$(window).innerHeight() - 100 + "px");
-	element.css("max-width",$(window).innerWidth() - 100 + "px");
-	element.css("top", ( $(window).innerHeight() - element.innerHeight() ) / 2+$(window).scrollTop() + "px");
-	element.css("left", ( $(window).innerWidth() - element.innerWidth() ) / 2+$(window).scrollLeft() + "px");
-	
+	element.css("position", "absolute");
+	element.css("max-height", $(window).innerHeight() - 100 + "px");
+	element.css("max-width", $(window).innerWidth() - 100 + "px");
+	element.css("top", ($(window).innerHeight() - element.innerHeight()) / 2 + $(window).scrollTop() + "px");
+	element.css("left", ($(window).innerWidth() - element.innerWidth()) / 2 + $(window).scrollLeft() + "px");
 	$(element).mCustomScrollbar();
 	
+	setTimeout(function(){ element.append('<span class="closeable" style="display:none;">I am closable</span>') }, 500);
 }
