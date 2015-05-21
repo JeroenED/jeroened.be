84a84a732a3dc7057e56f34ff7acb15eba91888a
Jeroen De Meerleer <me@jeroened.be>
2015-05-21 12:07:34 +0200

    Added JqueryUI confirmation boxes

--- src/JeroenED/CmsEDBundle/Resources/public/js/cmsed.js
+++ src/JeroenED/CmsEDBundle/Resources/public/js/cmsed.js
@@ -22,13 +22,24 @@
  * THE SOFTWARE.
  */
 $(document).ready(function() {
-    $(function() {
-            $( "#dialog" ).dialog({
-               autoOpen: false,  
-            });
-            $( "#showDialog a" ).click(function() {
-               $( "#dialog" ).dialog( "open" );
-            });
-         });
+	$("#dialog").dialog({
+		autoOpen: false,
+		modal: true
+	});
+	$(".confirmLink").click(function(e) {
+		$('#dialog').html($(this).data('text'));
+		e.preventDefault();
+		var targetUrl = $(this).data('link');
+		$("#dialog").dialog({
+			buttons: {
+				"Confirm": function() {
+					window.location.href = targetUrl;
+				},
+				"Cancel": function() {
+					$(this).dialog("close");
+				}
+			}
+		});
+		$("#dialog").dialog("open");
+	});
 });
-
