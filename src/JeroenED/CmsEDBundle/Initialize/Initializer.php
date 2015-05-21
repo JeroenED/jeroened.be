c180ac09d2ab7525586b7ed3e2a7c0b005bcdc5e
Jeroen De Meerleer <me@jeroened.be>
2015-05-21 12:59:04 +0200

    Fixed typo

--- src/JeroenED/CmsEDBundle/Initialize/Initializer.php
+++ src/JeroenED/CmsEDBundle/Initialize/Initializer.php
@@ -36,7 +36,7 @@ class Initializer {
         array('label' => 'Home', 'link' => '/admin', 'parts' => array()),
         array('label' => 'Portfolio', 'link' => '/admin/portfolio', 'parts' => array(array('label' => 'List', 'link' => '/admin/portfolio'), array('label' => 'Create new', 'link' => '/admin/portfolio/create'))),
         array('label' => 'Pages', 'link' => '/admin/pages', 'parts' => array(array('label' => 'List', 'link' => '/admin/pages'), array('label' => 'Create new', 'link' => '/admin/pages/create'))),
-        array('label' => 'Menu', 'link' => '/admin/menus', 'parts' => array(array('label' => 'List', 'link' => '/admin/menu'), array('label' => 'Create new', 'link' => '/admin/menu/create'))),
+        array('label' => 'Menu', 'link' => '/admin/menu', 'parts' => array(array('label' => 'List', 'link' => '/admin/menu'), array('label' => 'Create new', 'link' => '/admin/menu/create'))),
         array('label' => 'Users', 'link' => '/admin/users', 'parts' => array(array('label' => 'List', 'link' => '/admin/users'), array('label' => 'Create new', 'link' => '/admin/users/create'))),
     );
     
