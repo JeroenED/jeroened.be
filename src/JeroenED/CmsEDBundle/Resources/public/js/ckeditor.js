96c979dfd1a492077ff03c4a086ed772c6e1e581
Jeroen De Meerleer <me@jeroened.be>
2015-06-20 17:51:11 +0200

    fixed issue #9

---
+++ src/JeroenED/CmsEDBundle/Resources/public/js/ckeditor.js
@@ -0,0 +1,9 @@
+CKEDITOR.editorConfig = function(config) {
+   config.filebrowserBrowseUrl = '/admin/kcfinder/browse.php?opener=ckeditor&type=files';
+   config.filebrowserImageBrowseUrl = '/admin/kcfinder/browse.php?opener=ckeditor&type=images';
+   config.filebrowserFlashBrowseUrl = '/admin/kcfinder/browse.php?opener=ckeditor&type=flash';
+   config.filebrowserUploadUrl = '/admin/kcfinder/upload.php?opener=ckeditor&type=files';
+   config.filebrowserImageUploadUrl = '/admin/kcfinder/upload.php?opener=ckeditor&type=images';
+   config.filebrowserFlashUploadUrl = '/admin/kcfinder/upload.php?opener=ckeditor&type=flash';
+   config.allowedContent = true
+};
