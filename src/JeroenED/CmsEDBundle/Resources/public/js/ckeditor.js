dfc0901e0aada8e85cfa5136f46dadfcc7117242
Jeroen De Meerleer <me@jeroened.be>
2016-03-24 21:22:07 +0100

    Added stylesheet to CKeditor

--- src/JeroenED/CmsEDBundle/Resources/public/js/ckeditor.js
+++ src/JeroenED/CmsEDBundle/Resources/public/js/ckeditor.js
@@ -5,6 +5,7 @@ CKEDITOR.editorConfig = function(config) {
    config.filebrowserUploadUrl = '/admin/kcfinder/upload.php?opener=ckeditor&type=files';
    config.filebrowserImageUploadUrl = '/admin/kcfinder/upload.php?opener=ckeditor&type=images';
    config.filebrowserFlashUploadUrl = '/admin/kcfinder/upload.php?opener=ckeditor&type=flash';
-   config.allowedContent = true
+   config.allowedContent = true;
+   config.contentsCss = [ '/css/app.css', 'https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,300' ];
 };
 CKEDITOR.plugins.addExternal( 'codemirror', '/bundles/jeroenedcmsed/ckeditor-plugins/codemirror/', 'plugin.js' );
