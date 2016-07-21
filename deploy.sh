f6574f9be85cbcb3506d77798b8f38d254dbe67d
Jeroen De Meerleer <me@jeroened.be>
2016-07-21 14:32:17 +0200

    Fixed bug in CI

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
-src=${PWD}
-if [[ ${1} == "release" ]]; then
+
+if [[ ${CI_BUILD_REF_NAME} == "release" ]]; then
 	deployto=${release}	
 	cp -r ./* ${release}
-	delete=false
 else
 	deployto=${master}
 	cp -r ./* ${master}
-	delete=true
 fi
 
 cd ${deployto}
 composer install --no-dev --optimize-autoloader
 php bin/console assetic:dump
 php bin/console cache:clear
-if [[ ${delete} == true ]]; then
-	rm -rf ${src}
-fi
