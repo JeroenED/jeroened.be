8878d3c90a1719875e4ee57cee34533b2ba14389
Jeroen De Meerleer <me@jeroened.be>
2016-08-12 15:39:03 +0200

    Installed elFinder (#20)

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
             new JeroenED\CmsEDBundle\JeroenEDCmsEDBundle(),
             new lib\KCFinderBundle\libKCFinderBundle(),
+            new FM\ElfinderBundle\FMElfinderBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
